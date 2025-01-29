26b43c81845826c9cc29660732f008e85693c12c
Shiroko <ivanpalacios2809@gmail.com>
2025-01-29 03:42:54 -0600

    Add initial migration for User model with enums for Gender and Role

---
+++ prisma/migrations/20250129085015_init/migration.sql
@@ -0,0 +1,22 @@
+-- CreateEnum
+CREATE TYPE "Gender" AS ENUM ('FEMALE', 'MALE', 'OTHER');
+
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('ADMIN', 'USER');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "gender" "Gender" NOT NULL,
+    "age" INTEGER NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "Role" NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
