e2135394aa59d1bf59317755477a603f630787c0
Shiroko <ivanpalacios2809@gmail.com>
2025-01-30 01:01:11 -0600

    Add error handling for unexpected errors in user retrieval API

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -4,8 +4,11 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(req: NextRequest){
     try{
-    const users = await userService.getUsers(req);
-    return NextResponse.json({ 'message': users }, { status: 200 })
+    const result = await userService.getUsers(req);
+    if(result === Errors.ERROR_UNKNOWN){
+        return NextResponse.json({ 'error':'Un error inesperado ha ocurrido' }, { status: 500 })
+    }
+    return NextResponse.json({ 'message': result }, { status: 200 })
     } catch (error) {
         console.log(error)
         return NextResponse.json({ 'error': 'Un error inesperado ha ocurrido' }, { status: 500 })
