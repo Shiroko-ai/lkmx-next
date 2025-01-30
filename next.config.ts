a138bbecdfa171377633833714ae062ccb88d6e6
Shiroko <ivanpalacios2809@gmail.com>
2025-01-30 04:03:28 -0600

    Configured the build on docker-compose

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    webpack: (config: any, { dev }: any) => {
-      if (dev) {
-        config.watchOptions = {
-          poll: 1000,
-          aggregateTimeout: 300,
-        };
-      }
-      return config;
-    },
-  };
 
-  export default nextConfig;
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  output: 'standalone'
+};
+
+export default nextConfig;
