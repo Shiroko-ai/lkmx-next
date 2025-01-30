cb94274322249c32db5fb8c0643e8433d175fc17
Shiroko <ivanpalacios2809@gmail.com>
2025-01-29 21:00:31 -0600

    Add custom hook for data fetching with error handling and loading state

---
+++ app/components/hooks/userFetchData.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react'
+
+export default function useFetchData<T>(url: string) {
+    const [data, setData] = useState<T | null>(null)
+    const [isLoading, setIsLoading] = useState<boolean>(true)
+    const [error, setError] = useState<string | null>(null)
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try{
+                const response = await fetch(url)
+                const result = await response.json()
+                if(!response.ok){
+                    setError(result.error)
+                    throw new Error(result.error)
+                }
+                console.log(result)
+                setData(result.message)
+                setError(null)
+            }catch(error){
+                if(error instanceof Error){
+                setError(error.message)
+                }else{
+                    setError('An error occurred')
+                }
+            }
+            finally{
+                setIsLoading(false)
+            }
+        }
+
+        fetchData().catch(console.error)
+    },[url])
+
+    return { data, isLoading, error }
+}
