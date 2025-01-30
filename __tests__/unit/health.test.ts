89755fdeeb16e8b9e6fea9b4b2016e24a0a09f25
Shiroko <ivanpalacios2809@gmail.com>
2025-01-29 22:38:57 -0600

    Update health check test to use environment variable for API URL

--- __tests__/unit/health.test.ts
+++ __tests__/unit/health.test.ts
@@ -1,5 +1,8 @@
+import { loadEnvConfig } from '@next/env'
 it('Should be healthy and return a response with OK status', async () =>{
-    const response = await fetch('http://localhost:3000/api/health')
+    const projectDir = process.cwd()
+    loadEnvConfig(projectDir)
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/health`)
     const body = await response.json()
     expect(response.status).toBe(200)
     expect(body).toEqual({ message: 'Everything seems fine!' })
