3e2ca4ca672381c29f053c2abf70b7083f12a811
Shiroko <ivanpalacios2809@gmail.com>
2025-01-29 20:58:31 -0600

    Refactor API responses to standardize message format and enhance error handling for user creation

--- app/api/analytics/route.ts
+++ app/api/analytics/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from 'next/server';
 export async function GET(){
     try{
     const analytics = await userService.getAnalytics()
-    return NextResponse.json({ analytics }, { status: 200 })
+    return NextResponse.json({ message: analytics }, { status: 200 })
     }catch(e){
         console.log(e)
         return NextResponse.json({ 'error' : 'Un error inesperado ocurrió' }, { status: 500 })
--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -3,8 +3,8 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(req: NextRequest){
     try{
-    const result = await userService.getUsers(req);
-    return NextResponse.json({ result }, { status: 200 })
+    const users = await userService.getUsers(req);
+    return NextResponse.json({ 'message': users }, { status: 200 })
     } catch (error) {
         console.log(error)
         return NextResponse.json({ 'error': 'Un error inesperado ha ocurrido' }, { status: 500 })
@@ -15,7 +15,17 @@ export async function POST(req: NextRequest){
     const data = await req.json()
     try{
     const result = await userService.createUser(data);
-    return NextResponse.json({ result }, { status: 200 })
+    console.log(result)
+    if(result === 'ERROR UNKNOWN'){
+        return NextResponse.json({ 'error':'Un error inesperado ha ocurrido' }, { status: 500 })
+    }
+    if(result === 'ERROR VALIDATION'){
+        return NextResponse.json({ 'error':'Datos inválidos' }, { status: 400 })
+    }
+    if(result === 'ERROR REPEATED'){
+        return NextResponse.json({ 'error':'El correo ya existe, elige otro' }, { status: 400 })
+    }
+    return NextResponse.json({ 'message': 'Usuario creado con éxito' }, { status: 200 })
     }
     catch(error){
         console.log(error)
