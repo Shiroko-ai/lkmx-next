f1f817c807a695458ce2eaa3fea7ef7f16b9d9af
Shiroko <ivanpalacios2809@gmail.com>
2025-01-30 13:11:12 -0600

    Refactor Next.js configuration to enable standalone output and build

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,24 @@
 // This nextConfig is used to configure the Next.js build, when required uncomment the code below and comment the webpack one
-// import type { NextConfig } from 'next';
+import type { NextConfig } from 'next';
 
-// const nextConfig: NextConfig = {
-//   /* config options here */
-//   output: 'standalone'
-// };
+const nextConfig: NextConfig = {
+  /* config options here */
+  output: 'standalone'
+};
 
-// export default nextConfig;
+export default nextConfig;
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    webpack: (config: any, { dev }: any) => {
-      if (dev) {
-        config.watchOptions = {
-          poll: 1000,
-          aggregateTimeout: 300,
-        };
-      }
-      return config;
-    },
-  };
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     webpack: (config: any, { dev }: any) => {
+//       if (dev) {
+//         config.watchOptions = {
+//           poll: 1000,
+//           aggregateTimeout: 300,
+//         };
+//       }
+//       return config;
+//     },
+//   };
 
-  export default nextConfig;
+//   export default nextConfig;
